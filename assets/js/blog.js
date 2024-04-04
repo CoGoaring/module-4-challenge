3e57984fc498355620d61b32f7b853c45672ebd1
Connor <cguy33333@gmail.com>
2024-04-03 17:58:56 -0700

    Fixed the JS and made the blog posting fully functional.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,44 +5,55 @@ const username = JSON.parse(localStorage.getItem("username"));
 const title = JSON.parse(localStorage.getItem("title"));
 const content = JSON.parse(localStorage.getItem("content"));
 
-const blogs = [];
+// const blogs = [];
+const usernames = [];
+const titles = [];
+const contents = [];
 
 const getSavedBlogs = function() {
-    if (localStorage.getItem('blogs') !== null) {
-        blogs.push(JSON.parse(localStorage.getItem('blogs')));
+    // only need to check one since blogs cant be posted unless all values are filled
+    if (localStorage.getItem('usernames') !== null) {
+        usernames.push(JSON.parse(localStorage.getItem('usernames')));
+        titles.push(JSON.parse(localStorage.getItem('titles')));
+        contents.push(JSON.parse(localStorage.getItem('contents')));
     } else {/*Do Nothing*/}
+    usernames.push(username);
+    titles.push(title);
+    contents.push(content);
 }
-
-console.log(username);
-console.log(title);
-console.log(content);
 
 // create blog post with data 
 const renderBlogs = function () {
-    const blogSection = document.createElement("section");
-    const blogTitle = document.createElement("h3");
-    const blogContent = document.createElement("p");
-    const blogUsername = document.createElement("p");
+    // only need to check one since blogs cant be posted unless all values are filled
+    for (let i=0; i<usernames.length; i++) {
+        const blogSection = document.createElement("section");
+        const blogTitle = document.createElement("h3");
+        const blogContent = document.createElement("p");
+        const blogUsername = document.createElement("p");
 
-    blogTitle.textContent = title;
-    blogContent.textContent = content;
-    blogUsername.textContent = username;
+        blogTitle.textContent = titles[i];
+        blogContent.textContent = contents[i];
+        blogUsername.textContent = usernames[i];
 
-    blogSection.appendChild(blogTitle);
-    blogSection.appendChild(blogContent);
-    blogSection.appendChild(blogUsername);
+        blogSection.appendChild(blogTitle);
+        blogSection.appendChild(blogContent);
+        blogSection.appendChild(blogUsername);
 
-    blogs.push(blogSection);
-    localStorage.setItem(`blogs`, blogs);
-
-    for (let i=0; i<blogs.length; i++) {
-        mainEl.appendChild(blogs[i]);
+        console.log(blogSection);
+            
+        mainEl.appendChild(blogSection);
     }
     // mainEl.appendChild(blogSection);
 }
 
-
+const saveBlogs = function () {
+    localStorage.setItem('usernames', JSON.stringify(usernames));
+    localStorage.setItem('titles', JSON.stringify(titles));
+    localStorage.setItem('contents', JSON.stringify(contents));
+}
 
 getSavedBlogs();
 
 renderBlogs();
+
+saveBlogs();
